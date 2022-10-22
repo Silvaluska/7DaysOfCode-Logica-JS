8c2a72f4ff19ef9aab3f41ed1dbcf110e3a1571f
Lucas Silva <lucas030894@gmail.com>
2022-10-22 19:17:45 -0300

    update

--- Desafio 07/Desafio07.js
+++ Desafio 07/Desafio07.js
@@ -1,1 +1,32 @@
-var numero1 = 
+var list_numeros = document.querySelectorAll('.numeros');
+var operadores = document.querySelectorAll('.operadores');
+var funcoes = document.querySelectorAll('.funcoes');
+var numero1 = [];
+var numero2 = [];
+var operador;
+console.log(numero1)
+for (var i = 0; i < 10 ; i++) {
+    const id = i;
+    list_numeros[i].onclick = function() {
+        if (operador == null) {
+            numero1.push(list_numeros[id].innerHTML);
+        }
+        if (operador != null) {
+            numero2.push(list_numeros[id].innerHTML);
+        }
+    }
+    operadores[i].onclick = function() {
+        operador = operadores[id].innerHTML;
+    }
+    funcoes[i].onclick = function() {
+        if (funcoes[id] == '=') {
+            console.log('Calculando')
+        }
+        if (funcoes[id] == 'c') {
+            numero1 = [];
+            numero2 = [];
+            operador = [];
+        }
+    }
+}
+console.log(numero1)
