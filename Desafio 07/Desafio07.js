26b81e9cc56c5ecab4628876c94d623359193ede
Lucas Silva <lucas030894@gmail.com>
2022-10-24 14:13:39 -0300

    Update Desafio07.js

--- Desafio 07/Desafio07.js
+++ Desafio 07/Desafio07.js
@@ -1,32 +1,37 @@
 var list_numeros = document.querySelectorAll('.numeros');
 var operadores = document.querySelectorAll('.operadores');
 var funcoes = document.querySelectorAll('.funcoes');
-var numero1 = [];
-var numero2 = [];
+var numero1 = '';
+var numero2 = '';
 var operador;
 console.log(numero1)
 for (var i = 0; i < 10 ; i++) {
     const id = i;
     list_numeros[i].onclick = function() {
         if (operador == null) {
-            numero1.push(list_numeros[id].innerHTML);
+            numero1 += list_numeros[id].innerHTML;
         }
         if (operador != null) {
-            numero2.push(list_numeros[id].innerHTML);
-        }
-    }
-    operadores[i].onclick = function() {
-        operador = operadores[id].innerHTML;
-    }
-    funcoes[i].onclick = function() {
-        if (funcoes[id] == '=') {
-            console.log('Calculando')
-        }
-        if (funcoes[id] == 'c') {
-            numero1 = [];
-            numero2 = [];
-            operador = [];
+            numero2 += list_numeros[id].innerHTML;
         }
     }
 }
-console.log(numero1)
+for (var i = 0; i < 4 ; i++) {
+    const id = i;
+    operadores[i].onclick = function() {
+        operador = operadores[id].innerHTML;
+    }
+}
+for (var i = 0; i < 2 ; i++) {
+    const id = i;
+    funcoes[i].onclick = function() {
+        if (funcoes[id].innerHTML == '=') {
+            console.log('Calculando')
+        }
+        if (funcoes[id].innerHTML == 'c') {
+            numero1 = '';
+            numero2 = '';
+            operador = null;
+        }
+    }
+}
