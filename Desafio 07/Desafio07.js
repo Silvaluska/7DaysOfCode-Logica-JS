7a6cada435b7bad3d2c8716e7281a20b93686dd9
Lucas Silva <lucas030894@gmail.com>
2022-10-24 14:33:27 -0300

    Finalizando a calculadora

--- Desafio 07/Desafio07.js
+++ Desafio 07/Desafio07.js
@@ -4,15 +4,17 @@ var funcoes = document.querySelectorAll('.funcoes');
 var numero1 = '';
 var numero2 = '';
 var operador;
-console.log(numero1)
+var resposta = document.querySelector('.resposta');
 for (var i = 0; i < 10 ; i++) {
     const id = i;
     list_numeros[i].onclick = function() {
         if (operador == null) {
             numero1 += list_numeros[id].innerHTML;
+            resposta.innerHTML = numero1
         }
         if (operador != null) {
             numero2 += list_numeros[id].innerHTML;
+            resposta.innerHTML = numero1 + ' ' + operador + ' ' + numero2
         }
     }
 }
@@ -20,18 +22,32 @@ for (var i = 0; i < 4 ; i++) {
     const id = i;
     operadores[i].onclick = function() {
         operador = operadores[id].innerHTML;
+        resposta.innerHTML = numero1 + ' ' + operador
     }
 }
 for (var i = 0; i < 2 ; i++) {
     const id = i;
     funcoes[i].onclick = function() {
         if (funcoes[id].innerHTML == '=') {
-            console.log('Calculando')
+            if (operador == '+') {
+                resposta.innerHTML = parseInt(numero1) + parseInt(numero2) 
+            }
+            else if (operador == '-') {
+                resposta.innerHTML = parseInt(numero1) - parseInt(numero2)
+            }
+            else if (operador == 'x') {
+                resposta.innerHTML = parseInt(numero1) * parseInt(numero2)
+            }
+            else if (operador == '/') {
+                resposta.innerHTML = parseInt(numero1) / parseInt(numero2)
+            }
+            console.log(resposta)
         }
         if (funcoes[id].innerHTML == 'c') {
             numero1 = '';
             numero2 = '';
             operador = null;
+            resposta.innerHTML = ' '
         }
     }
 }
